21c71635e2a2326d1c7597458688e7135793a941
Alex Clevenger <abc324@lehigh.edu>
2024-09-17 23:24:05 -0400

    Modified install script

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ if ! apt install -y cmake g++ libcurl4-openssl-dev nlohmann-json3-dev; then
 fi
 
 # Build the project
-sudo ./build.sh
+source build.sh
 
 # Run CMake and check if it succeeds
 if ! cmake ..; then
